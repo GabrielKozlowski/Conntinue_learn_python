95c365ff573a4a2182017c35853309af9e41540f
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-11-22 15:54:35 +0100

    continue

--- lesson_one.py
+++ lesson_one.py
@@ -1,69 +1,89 @@
-a = b = c = 10
+# a = b = c = 10
 
-print(a,id(a),b,id(b),c,id(c))
+# print(a,id(a),b,id(b),c,id(c))
 
-a = 20
+# a = 20
 
-print(a,id(a),b,id(b),c,id(c))
-
-
-a = b = c = [1,2,3]
-
-print(a,id(a),b,id(b),c,id(c))
-
-a.append(4)
-
-print(a,id(a),b,id(b),c,id(c))
-
-x = 10
-y = 10
-
-print(x,id(x),y,id(y))
-
-y = y + 1 - 1
-
-print(x,id(x),y,id(y))
-
-y = y + 1234567890 - 1234567890
-
-print(x,id(x),y,id(y))
+# print(a,id(a),b,id(b),c,id(c))
 
 
-days = ['mon','tue','wed','thu','fri','sat','sun']
+# a = b = c = [1,2,3]
 
-worksday = days.copy()
+# print(a,id(a),b,id(b),c,id(c))
 
-worksday.remove('sun')
-worksday.remove('sat')
+# a.append(4)
 
-print(days,'\n',worksday)
+# print(a,id(a),b,id(b),c,id(c))
 
+# x = 10
+# y = 10
 
-options = """
-        1. load data
-        2. export data
-        3. analyze & predict
-        4. exit
-        """
+# print(x,id(x),y,id(y))
 
+# y = y + 1 - 1
 
-while True:
-    print(options)    
-    try:
-        choice = int(input('Chose number: '))
-    except ValueError:
-        print('Must write a number')
-    else:   
-        if choice == 1:
-            print('You choice load data')
-        elif choice == 2:
-            print('You choice export data')
-        elif choice == 3:
-            print('You choice analyze & predict')
-        elif choice == 4:
-            print('Good bye')
-            break
-        else:
-            print('You must choice a good number')
+# print(x,id(x),y,id(y))
+
+# y = y + 1234567890 - 1234567890
+
+# print(x,id(x),y,id(y))
 
 
+# days = ['mon','tue','wed','thu','fri','sat','sun']
+
+# worksday = days.copy()
+
+# worksday.remove('sun')
+# worksday.remove('sat')
+
+# print(days,'\n',worksday)
+
+
+# options = """
+#         1. load data
+#         2. export data
+#         3. analyze & predict
+#         4. exit
+#         """
+
+
+# while True:
+#     print(options)    
+#     try:
+#         choice = int(input('Chose number: '))
+#     except ValueError:
+#         print('Must write a number')
+#     else:   
+#         if choice == 1:
+#             print('You choice load data')
+#         elif choice == 2:
+#             print('You choice export data')
+#         elif choice == 3:
+#             print('You choice analyze & predict')
+#         elif choice == 4:
+#             print('Good bye')
+#             break
+#         else:
+#             print('You must choice a good number')
+
+
+import os
+
+
+plik = open('./plik.txt')
+
+def countWordsInText(parametr):
+    sample = parametr.read().split(' ')
+    words = len(sample)
+    return words
+    
+
+path = './plik.txt'
+
+if os.path.isfile(path): 
+    number = countWordsInText(plik)
+    print(number)
+
+
+
+os.path.isfile(path) and print(f'There are {number} words in the file "{path}"')
