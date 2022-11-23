b739c166886f496dba975a20a9e6b569d6f6e327
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-11-23 19:03:06 +0100

    dicts iteration, format string

---
+++ lesson_four.py
@@ -0,0 +1,25 @@
+banknotes_coins = [0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500]
+dict_denominations = {}
+
+
+for i in banknotes_coins:
+    dict_denominations.update({i:0})
+
+dict_denominations[100] += 1
+dict_denominations[20] += 1
+dict_denominations[5] += 1
+dict_denominations[0.5] += 1
+ 
+dict_denominations[50] += 1
+dict_denominations[20] += 2
+dict_denominations[5] += 1
+dict_denominations[2] += 2
+ 
+dict_denominations[100] += 1
+dict_denominations[50] += 1
+dict_denominations[2] += 1
+
+
+for k in dict_denominations:
+    print("Wartość nominału: %7.2f , Ilość: %1d, suma = %7.2f" % (k, dict_denominations[k], k * dict_denominations[k]))
+
