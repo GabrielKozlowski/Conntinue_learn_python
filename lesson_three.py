7fa33db24bdb928c71be82c6fb1fe0b5c080815e
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-11-23 16:57:00 +0100

    range,zip,enumerates function.

--- lesson_three.py
+++ lesson_three.py
@@ -26,31 +26,53 @@
 #     print('All pages added successfully')
 
 
-
-colors = ["red", "orange", "green", "violet", "blue", "yellow"]
-
-
-
-def numbersOfColors(colors,n):
-    colors = colors.copy()
-    return colors[:n]
+# #-----------------------------------------------------------
+# colors = ["red", "orange", "green", "violet", "blue", "yellow"]
 
 
-i = 0
-while i < len(colors):
-    numbersOfColors(colors,i+1)
-    i += 1
+
+# def numbersOfColors(colors,n):
+#     colors = colors.copy()
+#     return colors[:n]
 
 
-for j in range(1,len(colors)+1):
-    numbersOfColors(colors,j)
+# i = 0
+# while i < len(colors):
+#     numbersOfColors(colors,i+1)
+#     i += 1
 
 
-text = ''' 
-    Korporacja (z łac. corpo – ciało, ratus – szczur; pol. ciało szczura) – organizacja, która pod przykrywką prowadzenia
-    biznesu włada dzisiejszym światem. Wydawać się może utopijnym miejscem realizacji pasji zawodowych. W rzeczywistości 
-    jednak nie jest wcale tak kolorowo. Korporacja służy do wyzyskiwania człowieka w imię postępu. Rządzi w niej prawo dżungli.
-'''
+# for j in range(1,len(colors)+1):
+#     numbersOfColors(colors,j)
 
-print(text[text.index('(')+1:text.index(')')])
+
+# text = ''' 
+#     Korporacja (z łac. corpo – ciało, ratus – szczur; pol. ciało szczura) – organizacja, która pod przykrywką prowadzenia
+#     biznesu włada dzisiejszym światem. Wydawać się może utopijnym miejscem realizacji pasji zawodowych. W rzeczywistości 
+#     jednak nie jest wcale tak kolorowo. Korporacja służy do wyzyskiwania człowieka w imię postępu. Rządzi w niej prawo dżungli.
+# '''
+
+# print(text[text.index('(')+1:text.index(')')])
+
+
+# #-----------------------------------------------
+# projects = ['Brexit', 'Nord Stream', 'US Mexico Border']
+# leaders = ['Theresa May', 'Wladimir Putin', 'Donald Trump and Bill Clinton']
+
+
+# for i in list(zip(leaders,projects)):
+#     print("The leader of '",i[1],"' is ", i[0])
+
+
+# print('-'*50)
+
+# dates = ['2016-06-23', '2016-08-29', '1994-01-01']
+
+# for d, j in zip(dates,zip(leaders,projects)):
+#     print("The leader of '",j[1],"' started ",d, " is ", j[0])
+
+# print('-'*50)
+
+# for n, (d,j) in enumerate(zip(dates,zip(leaders,projects))):
+#     print(n+1,"- The leader of '",j[1],"' started ",d, " is ", j[0])
 
