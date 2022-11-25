ca9c81e77bda10f72da5ce92a7b7275d8a72709d
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-11-25 17:33:51 +0100

    compile function

--- lesson_five.py
+++ lesson_five.py
@@ -43,18 +43,61 @@
 
 # print(new_var)
 # #----------------------------------------------
-import os
+# import os
 
-files_to_process = [
-    r"C:\Users\FiFka9\Desktop\Nauka IT\Udemy\py_dla_srednio_zaawansowanych\script_lab_one.py",
-    r"C:\users\fiFka9\Desktop\Nauka IT\Udemy\py_dla_srednio_zaawansowanych\script_lab_two.py"
-    ]
+# files_to_process = [
+#     r"C:\Users\FiFka9\Desktop\Nauka IT\Udemy\py_dla_srednio_zaawansowanych\script_lab_one.py",
+#     r"C:\users\fiFka9\Desktop\Nauka IT\Udemy\py_dla_srednio_zaawansowanych\script_lab_two.py"
+#     ]
 
 
 
-for file_path in files_to_process:
+# for file_path in files_to_process:
 
-    with open(file_path, 'r') as f:
-        print('File: {}'.format(os.path.basename(file_path)))
-        source = f.read()
-        exec(source)
+#     with open(file_path, 'r') as f:
+#         print('File: {}'.format(os.path.basename(file_path)))
+#         source = f.read()
+#         exec(source)
+
+# #-----------------------------------------
+# # compile function, 
+# #-----------------------------------------
+
+import math
+import time
+
+formulas_list = [
+     "abs(x**3 - x**0.5)",
+     "abs(math.sin(x) * x**2)"
+     ]
+    
+argument_list = []
+
+for i in range (10000):
+    argument_list.append(i/10)
+
+
+for formula in formulas_list:
+    results_list = []
+    print('Formula: {}'.format(formula))
+    
+    start = time.time()
+    for x in argument_list:
+        results_list.append(eval(formula))
+    print('Minimum value: {}. Maxiumum value: {}'.format(min(results_list),max(results_list)))
+    stop = time.time()
+    print('Nie kompilowane zajęło: ', stop-start)
+    print('\n')
+print('*' * 40)
+for formula in formulas_list:
+    results_list = []
+    print('Formula: {}'.format(formula))
+    
+    start = time.time()
+    compiled_formula = compile(formula, formula, 'eval')
+    for x in argument_list:
+        results_list.append(eval(compiled_formula))
+    print('Minimum value: {}. Maxiumum value: {}'.format(min(results_list),max(results_list)))
+    stop = time.time()
+    print('Nie kompilowane zajęło: ', stop-start)
+    print('\n')
