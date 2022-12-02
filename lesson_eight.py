2742511b676000df43c2a75eaccd82f6102b5d7a
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-02 16:29:44 +0100

    Decorator for functions

--- lesson_eight.py
+++ lesson_eight.py
@@ -1,11 +1,45 @@
 import time
 
 
+# # **Tworzenie wrappera dla każdej funkcji wpisanej w argument **
+
+# def wrapper_time(a_function):
+#     def a_wrapped_function(*args, **kwargs):
+#         time_start = time.time()
+#         v = a_function(*args, **kwargs)
+#         time_stop = time.time()
+#         print("Function {} was done in time {}".format(a_function.__name__, (time_stop-time_start)))
+#         return v
+#     return a_wrapped_function
+
+
+
+
+# def get_sequence(n):
+    
+#     if n <= 0:
+#         return 1
+#     else:
+#         v = 0
+#         for i in range(n):
+#             v += 1 + (get_sequence(i - 1) + get_sequence(i))/2
+#         return v
+
+
+# wrapper_get_sequence = wrapper_time(get_sequence)
+
+# print(wrapper_get_sequence(5))
+
+
+#** Użycie dekoratora do wrappera dla funkcji  **
+
+import functools
+
 
 def wrapper_time(a_function):
     def a_wrapped_function(*args, **kwargs):
         time_start = time.time()
-        v = a_function(5)
+        v = a_function(*args, **kwargs)
         time_stop = time.time()
         print("Function {} was done in time {}".format(a_function.__name__, (time_stop-time_start)))
         return v
@@ -13,7 +47,7 @@ def wrapper_time(a_function):
 
 
 
-
+@wrapper_time
 def get_sequence(n):
     
     if n <= 0:
@@ -24,7 +58,4 @@ def get_sequence(n):
             v += 1 + (get_sequence(i - 1) + get_sequence(i))/2
         return v
 
-
-wrapper_get_sequence = wrapper_time(get_sequence)
-
-print(wrapper_get_sequence(18))
+print(get_sequence(2))
