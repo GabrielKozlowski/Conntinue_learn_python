90ffa2549a8229e4e4624a4cd32d54422b671108
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-05 17:26:27 +0100

    learn lambda

--- lesson_ten.py
+++ lesson_ten.py
@@ -1,23 +1,35 @@
-import functools
-import time
+# import functools
+# import time
 
-@functools.lru_cache(100)
-def fib(n):
+# @functools.lru_cache(maxsize=100)
+# def fib(n):
     
-    if n < 2:
-        result = n
-    else:
-        result = fib(n-1) + fib(n-2)
+#     if n < 2:
+#         result = n
+#     else:
+#         result = fib(n-1) + fib(n-2)
         
-    return result
+#     return result
 
 
 
-start = time.time()
-for i in range(1,33):
-    fib(i)
-    stop = time.time()
-    timeRemind = stop-start
-    print('Number :{} it tooks {} s'.format(i,timeRemind))
+# start = time.time()
+# for i in range(1,33):
+#     fib(i)
+#     stop = time.time()
+#     timeRemind = stop-start
+#     print('Number :{} it tooks {} s'.format(i,timeRemind))
 
-print(fib.cache_info())
+# print(fib.cache_info())
+
+
+text_list = ['x','xxx','xxxxx','xxxxxxx','']
+
+
+f = lambda x: len(x)
+
+print(f('this is test text'))
+
+print(list(map(f,text_list)))
+
+print(list(map(lambda x: len(x),text_list)))
