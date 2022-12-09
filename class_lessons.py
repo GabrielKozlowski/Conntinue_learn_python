24abb493558f2f71c259c71fece741ab9c4d66a9
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-09 13:09:27 +0100

    adding task of class

---
+++ class_lessons.py
@@ -0,0 +1,26 @@
+
+# class OldPhoneShop:
+
+#     def __init__(self, brand, model, year, processor, screen_size, phone_memory, ram, rear_camera, price) -> None:
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+#         self.processor = processor
+#         self.screen_size = screen_size
+#         self.phone_memory = phone_memory
+#         self.ram = ram
+#         self.rear_camera = rear_camera
+#         self.price = price
+
+
+# phone_1 = OldPhoneShop('Samsung', 'S5', 2014, 'Qualcom Snapdragon 801', '5.1"', '16GB', '2GB', '16 Mpix', '100$')
+# phone_2 = OldPhoneShop('Xiaomi', 'Redmi 5', 2017, 'Qualcomm Snapdragon 450', '5.7"', '16GB', '2GB', '12 Mpix', '180$')
+# phone_3 = OldPhoneShop('Apple', 'iPhone 5', 2012, 'Apple A6', '4.0"', '32GB', '1GB', '8 Mpix', '105$')
+
+# phones = [phone_1,phone_2,phone_3]
+# print("Todays sale:\n")
+# for fon in phones:
+#     print("Phone: {} -- {}\nYear: {}\nProcessor: {}\nScreen size: {}, Ram: {}\nPhone memory: {}, Camera: {}\nCosts: {}\n".format\
+#         (fon.brand, fon.model, fon.year, fon.processor, fon.screen_size, fon.ram, fon.phone_memory, fon.rear_camera, fon.price))
+
+# # *****************************************************************************************************************
