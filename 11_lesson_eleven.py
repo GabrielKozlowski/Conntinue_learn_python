b22041a0b22e090cb41ca08efee834d523f2795d
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-10 19:41:57 +0100

    Continue with class

--- 11_lesson_eleven.py
+++ 11_lesson_eleven.py
@@ -31,27 +31,86 @@
 # # Create a class for task.
 
 
-class Cake:
+# class Cake:
 
-    def __init__(self, name, kind, taste, additves, filling) -> None:
-        self.name = name
-        self.kind = kind
-        self.taste = taste
-        self.additves = additves
-        self.filling = filling
-
-
-cake_1 = Cake('Apple_pie', 'cake with apples', 'apple', ['castor sugar'], [])
-cake_2 = Cake('Pickaninny_pie', 'black cake', 'cacao', ['icing'], ['chocolade'])
+#     def __init__(self, name, kind, taste, additves, filling) -> None:
+#         self.name = name
+#         self.kind = kind
+#         self.taste = taste
+#         self.additves = additves
+#         self.filling = filling
 
 
-print(cake_1.name, cake_1.kind, cake_1.taste, cake_1.additves, cake_1.filling)
-print("My favorite cake is {}, it looks like {} and have taste {}. On top it haves {}.{} ".format(cake_2.name, cake_2.kind, cake_2.taste, cake_2.additves, cake_2.filling))
+# cake_1 = Cake('Apple_pie', 'cake with apples', 'apple', ['castor sugar'], [])
+# cake_2 = Cake('Pickaninny_pie', 'black cake', 'cacao', ['icing'], ['chocolade'])
+
+
+# print(cake_1.name, cake_1.kind, cake_1.taste, cake_1.additves, cake_1.filling)
+# print("My favorite cake is {}, it looks like {} and have taste {}. On top it haves {}.{} ".format\
+#     (cake_2.name, cake_2.kind, cake_2.taste, cake_2.additves, cake_2.filling))
 
 
 
-bakery_offer = [cake_1,cake_2]
+# bakery_offer = [cake_1,cake_2]
 
-for c in bakery_offer:
-    print("Today in our offer:\n{} - ({}) main taste: {} with additives of\n{}, filled with {}.\n".format(c.name, c.kind, c.taste, c.additves, c.filling))
+# for c in bakery_offer:
+#     print("Today in our offer:\n{} - ({}) main taste: {} with additives of\n{}, filled with {}.\n".format\
+#         (c.name, c.kind, c.taste, c.additves, c.filling))
+
+# #**********************************************************************************************************************
+
+
+
+# class Cake:
+
+#     def __init__(self, name, kind, taste, additves, filling) -> None:
+#         self.name = name
+#         self.kind = kind
+#         self.taste = taste
+#         self.additves = additves
+#         self.filling = filling
+
+#     def show_info(self):
+#         print('{}'.format(self.name).upper())
+#         print('Taste: {}'.format(self.taste))
+#         if len(self.additves) > 0:
+#                 print('Additves: \n\t{}'.format(self.additves))
+        
+#         print('Filling: {}'.format(self.filling))
+
+#     def set_filling(self, filling):
+#         self.filling += filling
+
+#     def add_additives(self, additves):
+#         self.additves += '\n\t' + additves 
+        
+
+
+# cake_1 = Cake('Apple_pie', 'cake with apples', 'apple', 'castor sugar', '')
+# cake_2 = Cake('Pickaninny_pie', 'black cake', 'cacao', 'icing', 'chocolade')
+# cake_3 = Cake('Chocolade Muffin','muffin', 'chocolade', 'chocolade', '')
+# print('-'* 50)
+
+# cake_1.show_info()
+# print('-'* 50)
+# cake_2.show_info()
+# print('-'* 50)
+# cake_1.set_filling('apples')
+# cake_1.set_filling('crumble')
+# cake_1.show_info()
+# print('-'* 50)
+# cake_1.add_additives('cinnamon')
+# cake_1.add_additives('brown sugar')
+# cake_2.add_additives('nuts')
+# cake_3.add_additives('cream')
+# cake_1.show_info()
+# print()
+# cake_2.show_info()
+# print()
+# cake_3.show_info()
+# print('-'* 50)
+
+# #****************************************************************
+
+
 
