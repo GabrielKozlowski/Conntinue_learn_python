b1bad30e8e59f00918fb53b87a13e935144176fc
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-23 15:33:30 +0100

    Some practice with self reported bugs

--- 15_lesson_fifteen.py
+++ 15_lesson_fifteen.py
@@ -102,6 +102,54 @@
 #******************************************************************************************
 
 
+# import datetime as dt
+ 
+# class Trip:
+#     def __init__(self, symbol, title, start, end):
+#         self.symbol = symbol
+#         self.title = title
+#         self.start = start
+#         self.end = end
+ 
+#     def check_data(self):
+#         if len(self.title) == 0:
+#             raise Exception("Title is empty!")
+#         if self.start > self.end:
+#             raise ValueError("Start date is later than end date!")
+ 
+#     @classmethod
+#     def publish_offer(cls, trips):
+#         list_of_errors = []
+
+#         for trip in trips:
+
+#             try:
+#                 trip.check_data()
+#             except ValueError as e:
+#                 list_of_errors.append(f"{trip.symbol} : {str(e)}")
+#             except Exception as e:
+#                 list_of_errors.append(f"{trip.symbol} : {str(e)}")
 
 
+#         if len(list_of_errors) > 0:
+#             raise Exception(f"The list of trips has errors:\n{list_of_errors}")
+#         else:
+#             print("The offer will be published...")
+
+
+# trips = [
+#             Trip('IT-VNC', 'Italy-Venice', dt.date(2023, 6, 1), dt.date(2023, 6, 12)),
+#             Trip('SP-BRC', 'Spain-Barcelona', dt.date(2023, 6, 12), dt.date(2023, 5, 22)),
+#             Trip('IT-ROM', 'Italy-Rome', dt.date(2023, 6, 21), dt.date(2023, 6, 12))
+#         ]
+
+# try:
+#     print("Start checking trips ")
+#     Trip.publish_offer(trips)
+#     print("Done.")
+
+# except Exception as e:
+#     print(f"There is an error with trips \nError:\n{e}")
+
+# #***********************************************************************************************************
 
