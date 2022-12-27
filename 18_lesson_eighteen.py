0370077d3abb79678c34c25b206aae5993d1f9b7
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-27 21:39:56 +0100

    method __getitem__

--- 18_lesson_eighteen.py
+++ 18_lesson_eighteen.py
@@ -1,6 +1,5 @@
 
-import time
- 
+
 
 class Combinations:
 
@@ -9,44 +8,45 @@ class Combinations:
         self.promotions = promotions
         self.customers = customers
         
-        self.current_product = 0
-        self.current_promotion = 0 
-        self.current_customer = 0
 
-    def __next__(self):
-        if self.current_customer >= len(self.customers):
-            self.current_customer = 0
-            self.current_promotion += 1
-
-        if self.current_promotion >= len(promotions):
-            self.current_promotion = 0
-            self.current_product += 1
-
-        if self.current_product >= len(self.products):
-            self.current_product = 0
-            raise StopIteration
     
-        item_to_return = "{} - {} - {}".format(self.products[self.current_product], self.promotions[self.current_promotion], self.customers[self.current_customer])
-        self.current_customer += 1
-        return item_to_return
+    def __getitem__(self, item):
 
-    def __iter__(self):
-        return self
+       
+        if item >= len(self.products) * len(self.promotions) * len(self.customers):
+            raise StopIteration
+        else:
+            pos_products = item // (len(self.promotions) * len(self.customers))
+            item = item % (len(self.promotions) * len(self.customers))
 
-products = ["Product {}".format(i) for i in range(1, 501)]
-print(products)
- 
-promotions = ["Promotion {}".format(i) for i in range(1, 51)]
-print(promotions)
- 
-customers = ['Customer {}'.format(i) for i in range(1, 501)]
-print(customers)
- 
+            pos_promotions = item // len(self.customers)
+            item = item % len(self.customers)
 
+            pos_customers = item
+
+        return "{} - {} - {}".format(self.products[pos_products], self.promotions[pos_promotions], self.customers[pos_customers])
+
+
+
+products = ["Product {}".format(i) for i in range(1, 4)]
+# print(products)
+ 
+promotions = ["Promotion {}".format(i) for i in range(1, 3)]
+# print(promotions)
+ 
+customers = ['Customer {}'.format(i) for i in range(1, 6)]
+# print(customers)
+ 
 combinations = Combinations(products, promotions, customers)
+
+
+# for i in range(0, 31):
+#     print(i, combinations[i])
+
+combinations_iterator = iter(combinations)
+print(next(combinations_iterator))
+
+
+for c in combinations_iterator:
+    print(c)
  
-for c in combinations:
-    # here an analysis will be done - currently, just nothing happens :)
-    pass
- 
-time.sleep(10)
