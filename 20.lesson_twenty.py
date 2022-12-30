b766e49466b544a1e1cc6bd30f2790398445d4c8
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-30 13:18:50 +0100

    itertools combinations,product,permutations

---
+++ 20.lesson_twenty.py
@@ -0,0 +1,19 @@
+
+# import itertools as it
+# import math
+
+# notes = ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+
+# for a in it.permutations(notes, 4):
+#     print(a)
+
+
+# print("4-notes melody with not repeat. Can be created {} melodys.".format(int(math.factorial(len(notes)) / math.factorial((len(notes) - 4)))))
+
+
+# for b in it.product(notes, repeat=4):
+#     print(b)
+
+# print("4-notes melody with repeats. Can be created {} melodys.".format(int(math.pow(len(notes), 4))))
+
+# #***********************************************************************************************************
