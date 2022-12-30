968c44f382501a969a0a5fa8cef49703df5f84f3
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-30 14:29:48 +0100

    itertools.groupby - data grouping

--- 20.lesson_twenty.py
+++ 20.lesson_twenty.py
@@ -17,3 +17,31 @@
 # print("4-notes melody with repeats. Can be created {} melodys.".format(int(math.pow(len(notes), 4))))
 
 # #***********************************************************************************************************
+
+import os
+import itertools as it
+
+def scantree(path):
+    for entry in os.scandir(path):
+        if entry.is_dir():
+            yield entry
+            yield from scantree(entry.path)
+        else:
+            yield entry
+
+
+listing = scantree('./links_to_check/')
+
+
+for l in listing:
+    print(f"{l} is dir" if l.is_dir() else f"{l} is file")
+
+
+listing = scantree('./links_to_check/')
+listing = sorted(listing, key=lambda e: e.is_dir())
+
+
+for is_dir, elements in it.groupby(listing, key = lambda e: e.is_dir()):
+    print('Is dir ' if is_dir else 'Is file', len(list(elements)))
+
+
