084f031862bb26f9e91a04f4b92fb3cb404e8748
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-30 12:16:09 +0100

    generator exercises

--- 19_lesson_nineteen.py
+++ 19_lesson_nineteen.py
@@ -17,27 +17,71 @@
 
 # #*****************************************************************************
 
-import random
+# import random
 
-def random_with_sum(numbers_of_values, asserted_sum):
+# def random_with_sum(numbers_of_values, asserted_sum):
 
-    trial = 0
-    numbers = list(range(numbers_of_values))
+#     trial = 0
+#     numbers = list(range(numbers_of_values))
 
-    while True:
-        trial += 1
+#     while True:
+#         trial += 1
 
-        for i in range(numbers_of_values):
-            numbers[i] = (random.randint(1, 101))
+#         for i in range(numbers_of_values):
+#             numbers[i] = (random.randint(1, 101))
 
-        if sum(numbers) == asserted_sum:
-            yield (trial, numbers)
-            trial = 0
+#         if sum(numbers) == asserted_sum:
+#             yield (trial, numbers)
+#             trial = 0
 
-for i in range(10):
-    (number_of_trials, numbers) = next(random_with_sum(3, 100))
-    print(number_of_trials, numbers)
+# for i in range(10):
+#     (number_of_trials, numbers) = next(random_with_sum(3, 100))
+#     print(number_of_trials, numbers)
 # #*****************************************************************************
 
+# import os 
+# import requests
+
+# def gen_get_files(dir):
+#     for file in os.listdir(dir):
+#         yield os.path.join(dir, file)
+
+
+# def gen_get_file_lines(filename):
+#     with open(filename, 'r') as f:
+#         for line in f.readlines():
+#             yield line.replace('\n',  '')
+
+
+# def check_webpage(url):
+
+#     try:
+#         response = requests.get(url)
+#         return response.status_code == 200
+#     except:
+#         return False
+
+
+# try:
+#     os.mkdir('./links_to_check')
+# except:
+#     pass
+
+# with open('./links_to_check/pl.txt', 'w') as f:
+#     f.write('http://www.wykop.pl/\n')
+#     f.write('http://www.ale-beka-jest-taki-adres.pl/\n')
+#     f.write('http://www.demotywatory.pl/\n')
+
+# with open('./links_to_check/com.txt', 'w') as f:
+#     f.write('http://www.realpython.com/\n')
+#     f.write('http://www.nonexistenturl.com/\n')
+#     f.write('http://www.stackoverflow.com/\n')
+
+
+# for file in gen_get_files('./links_to_check'):
+#     for line in gen_get_file_lines(file):
+#         print("{} - {} - {}".format(file, line, check_webpage(line)))
+        
+# #*****************************************************************************
 
 
