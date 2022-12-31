f42c0f5d0f5043d6cd9dd85893bedc85e2074d8b
Gabrielkozlowski <gabriel89krk@gmail.com>
2022-12-31 14:28:15 +0100

    context manager

---
+++ 21_lesson_twentyone.py
@@ -0,0 +1,26 @@
+
+# class HtmlCM:
+
+#     def __init__(self):
+#         pass
+
+#     def __enter__(self,):
+#         print('<TABLE>')
+#         print('<TR>')
+#         print('\t<TH>Number</TH><TH>Description</TH>')
+#         print('</TR>')
+#         return self
+
+#     def __exit__(self, type, value, traceback):
+#         print('</TABLE>')
+
+# with HtmlCM() as html:
+    
+#     print('<TR>')
+#     print('\t<TD>1</TD><TD>Say hello!</TD')
+#     print('</TR>')
+#     print('<TR>')
+#     print('\t<TD>2</TD><TD>Say good bye!</TD')
+#     print('</TR>')
+
+# #*****************************************************************
